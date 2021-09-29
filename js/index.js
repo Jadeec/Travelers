62e5d9aefd7cc7a6fc2f3ed081a81f1772584e36
innermost47 <anthony.charretier@gmail.com>
2021-09-29 17:04:15 +0200

    refactor navbar to render it portable for an other project

--- js/index.js
+++ js/index.js
@@ -29,7 +29,11 @@ const menus = [
     menu: "oceania",
     link: "oceania.html"
   },
-]
+];
+
+const imageMenuSrc = "./assets/compass.svg";
+const imageMenuAlt = "find the north";
+const menuTitle = "TRAVELERS";
 
 const navbar = document.querySelector(".navbar");
 const navbarNav = document.createElement("nav");
@@ -53,14 +57,14 @@ navbarNav.appendChild(navbarTitleLink);
 
 const navbarTitle = document.createElement("h2");
 navbarTitle.classList.add("navbar--title");
-navbarTitle.innerHTML = "TRAVELERS";
+navbarTitle.innerHTML = menuTitle;
 navbarTitleLink.appendChild(navbarTitle);
 
-const compass = document.createElement("img");
-compass.src = "./assets/compass.svg";
-compass.alt = "find the north";
-compass.classList.add("navbar--image");
-navbarNav.appendChild(compass);
+const image = document.createElement("img");
+image.src = imageMenuSrc;
+image.alt = imageMenuAlt;
+image.classList.add("navbar--image");
+navbarNav.appendChild(image);
 
 const navbarUl = document.createElement("ul");
 navbarUl.classList.add("navbar--ul");
