b533e1c2e15142ba6794eed71fc5f906d64c9fc8
camille <camfrelat@gmail.com>
2021-09-30 16:28:32 +0200

    add lazy load feature for pictures on pages and added gitignore

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("it works");
+const images = document.querySelectorAll('img');
+
+let imgObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        const image = entry.target;
+        const newSrc = image.getAttribute('data-src')
+        image.src = newSrc;
+        console.log(image);
+        imgObserver.unobserve(entry.target);
+    });
+});
+
+images.forEach((image) => {
+    imgObserver.observe(image);
+})
