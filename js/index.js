ffa0b75d91d17f8f4f4a05be41954db74d3cf53b
camille <camfrelat@gmail.com>
2021-10-06 12:30:03 +0200

    add slide right and left on pictures on scroll/load

--- js/index.js
+++ js/index.js
@@ -170,14 +170,17 @@ const images = document.querySelectorAll('.pagesImages');
 
 const imgObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (!entry.isIntersecting) return;
-    const image = entry.target;
-    const newSrc = image.getAttribute('data-src');
-    image.src = newSrc;
-    imgObserver.unobserve(entry.target);
+    const img = entry.target;
+    if (entry.intersectionRatio > 0) {
+      const newSrc = img.getAttribute('data-src');
+      img.src = newSrc;
+      img.classList.add('slide');
+    } else {
+      img.classList.remove('slide');
+    }
   });
 });
 
-images.forEach((image) => {
-  imgObserver.observe(image);
+images.forEach((element) => {
+  imgObserver.observe(element);
 });
